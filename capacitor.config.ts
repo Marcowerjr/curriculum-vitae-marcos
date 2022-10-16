76a37ebae34cc39316a9784d006d7b5d5cfb2202
Marcos Caballero Fernández <marcowerjr@gmail.com>
2022-10-16 16:31:47 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'curriculum-vitae-marcos',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
