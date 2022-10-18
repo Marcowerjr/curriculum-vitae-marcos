04157a3b48c145e6150f71406558b33579038455
Marcos Caballero Fernández <marcowerjr@gmail.com>
2022-10-17 20:38:53 -0600

    Update tabs pages, and use components previously created

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,10 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  grades = [
+    {
+      title:
+        'Licenciatura en Ingeniería en Desarrollo de Software: Desarrollo y Gestión de Software',
+      subtitle:
+        'Universidad Tecnológica de Chihuahua - Chihuahua, CHH, En curso',
+      logo: 'assets/img/escudo.webp',
+      description:
+        ' Estudios no finalizados en Desarrollo y Gestión de Software en Universidad Tecnológica de Chihuahua',
+    },
+    {
+      title:
+        'Grado Técnico Superior en Desarrollo de Software: Desarrollo de software multiplataforma',
+      subtitle:
+        'Universidad Tecnológica de Chihuahua - Chihuahua, CHH, Agosto del 2021',
+      logo: 'assets/img/escudo.webp',
+      description:
+        'Promedio general de 10, dos proyectos integradores realizados con 10 de calificación',
+    },
+  ];
 
   constructor() {}
-
 }
