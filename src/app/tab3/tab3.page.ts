fb826f313bdf572aa069528d32ff25f5aefcebf2
Marcos Caballero Fernández <marcowerjr@gmail.com>
2022-11-07 11:48:15 -0600

    Fix columns and rows in desktop mode

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -8,18 +8,24 @@ import { Component } from '@angular/core';
 export class Tab3Page {
   works = [
     {
-      companyName: 'Arvolution',
-      companyLogo: 'assets/img/arvolution.png',
-      position: 'Desarrollador full stack',
+      companyName: 'Codanai Software',
+      companyLogo: 'assets/img/codanai.png',
+      position: 'Desarrollador back end',
       description: [
-        `Desarrollo de API's con Node y Express utilizando TypeScript, Sequelize, GraphQL y MySQL`,
-        `Desarrollo de aplicaciones web con React, TypeScript, GraphQL y Apollo`,
-        `Desarrollo de aplicaciones móviles con React Native, TypeScript y GraphQL`,
-        `Manejo de versiones con Git`,
+        `Realización de un sistema a reparto a domicilio, con funcionalidades acerca de creación, 
+        modificación y eliminación de repartidores, domicilios de entrega y fechas de entrega.`,
+        `Tengo experiencia en trabajar con equipos de trabajo que utilizan la metodología SCRUM.`,
+        `Experiencia en Clean Code.`,
+        `Cooperación con los desarrolladores front-end para la integración de nuevos elementos 
+        orientados al usuario.`,
+        `Escritura de código para desarrollar aplicaciones web funcionales.`,
+        `Elaboración de la documentación técnica de las aplicaciones desarrolladas.`,
+        `Diseño y escritura de API utilizando Express.js.`,
+        `Aplicación de estándares de diseño, codificación y documentación.`,
       ],
       location: 'Chihuahua, CHH',
-      startDate: 'Marzo del 2022',
-      endDate: 'Actualidad',
+      startDate: 'Mayo del 2021',
+      endDate: 'Octubre del 2021',
     },
     {
       companyName: 'Grupo Lifandi',
@@ -36,24 +42,18 @@ export class Tab3Page {
       endDate: 'Noviembre del 2021',
     },
     {
-      companyName: 'Codanai Software',
-      companyLogo: 'assets/img/codanai.png',
-      position: 'Desarrollador back end',
+      companyName: 'Arvolution',
+      companyLogo: 'assets/img/arvolution.png',
+      position: 'Desarrollador full stack',
       description: [
-        `Realización de un sistema a reparto a domicilio, con funcionalidades acerca de creación, 
-        modificación y eliminación de repartidores, domicilios de entrega y fechas de entrega.`,
-        `Tengo experiencia en trabajar con equipos de trabajo que utilizan la metodología SCRUM.`,
-        `Experiencia en Clean Code`,
-        `Cooperación con los desarrolladores front-end para la integración de nuevos elementos 
-        orientados al usuario.`,
-        `Escritura de código para desarrollar aplicaciones web funcionales.`,
-        `Elaboración de la documentación técnica de las aplicaciones desarrolladas.`,
-        `Diseño y escritura de API utilizando Express.js.`,
-        `Aplicación de estándares de diseño, codificación y documentación.`,
+        `Desarrollo de API's con Node y Express utilizando TypeScript, Sequelize, GraphQL y MySQL.`,
+        `Desarrollo de aplicaciones web con React, TypeScript, GraphQL y Apollo.`,
+        `Desarrollo de aplicaciones móviles con React Native, TypeScript y GraphQL.`,
+        `Manejo de versiones con Git.`,
       ],
       location: 'Chihuahua, CHH',
-      startDate: 'Mayo del 2021',
-      endDate: 'Octubre del 2021',
+      startDate: 'Marzo del 2022',
+      endDate: 'Actualidad',
     },
   ];
 
